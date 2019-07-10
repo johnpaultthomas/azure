db6f42b39e1ec2b7d9c4c27b70bb308322bfeb0a
johnpaul <johnpaultthomas@yahoo.co.in>
2019-07-11 10:06:12 +1200

    Create AzureSchedulers_DataDump.ps1

---
+++ AzureSchedulers_DataDump.ps1
@@ -0,0 +1,47 @@
+param (
+    [string] $SubcriptionID = "",
+    [string] $ResourceGroup = "",
+)
+
+# If you are not already authenticated to Azure
+# Connect-AzureRMAccount
+
+
+Select-AzureRMSubscription -SubscriptionId $SubcriptionID
+
+
+$JobCollections=Get-AzureRmSchedulerJobCollection -ResourceGroupName $ResourceGroup
+
+
+$jc=Get-AzureRmSchedulerJob -ResourceGroupName $ResourceGroup -JobCollectionName $JobCollections.JobCollectionName[0]
+
+ForEach($jcname in $JobCollections.JobCollectionName){
+$jc = Get-AzureRmSchedulerJob -ResourceGroupName $ResourceGroup -JobCollectionName $jcname
+$k=0
+
+    ForEach ($i in $jc)
+    {
+       $k=$k+1
+       Try
+        {
+                if($i.JobAction.JobActionType -eq "ServiceBusQueue")
+                {
+                    ($jcname + "," + $k.tostring() + "," + $i.JobName.ToString() + "," + $i.JobAction.ServiceBusMessage.ToString() + "," + $i.JobAction.ServiceBusQueueName.ToString() + "," + $i.Recurrence.ToString()) >> schedulers_servicebus.csv
+                }
+      
+                else 
+                {
+                    ($k.tostring() + "," + $jcname + "," + $i.JobName.ToString() + "," + $i.JobAction.RequestMethod.ToString() + "," + $i.JobAction.URI.ToString() + "," + $i.Recurrence.ToString())  >> schedulers_http.csv
+                }
+        }
+        Catch
+        {
+              $i.JobName
+              $i.JobAction.RequestMethod 
+              $i.JobAction.URI 
+        }
+   }
+
+}
+
+
