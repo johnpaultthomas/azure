1f31e068aa0ba6c8d8de27a8a86f8a49aa31a8a9
Johnpaul Thomas <johnpaul.thomas@Johnpauls-MacBook-Pro-2.local>
2019-07-12 16:17:54 +1200

    modified

--- SchedulerMigrationtoFunction/EveryMinute/run.ps1
+++ SchedulerMigrationtoFunction/EveryMinute/run.ps1
@@ -3,7 +3,7 @@ param($Timer,[String[]] $InputBlob)
 
 # Get the current universal time in the default string format
 $currentUTCtime = (Get-Date).ToUniversalTime()
-
+$originalUTCTime= (Get-Date -Year 2019 -Month 1 -Day 1 -Hour 0 -minute 0 -second 0).ToUniversalTime()
 # The 'IsPastDue' porperty is 'true' when the current function invocation is later than scheduled.
 if ($Timer.IsPastDue) {
     Write-Host "PowerShell timer is running late!"
@@ -18,10 +18,11 @@ foreach ($job in $jobs){
         $jobName       = $job.split(",")[1]
         $uri           = $job.split(",")[2]
         $requestMethod = $job.split(",")[3]
-        
-        $response= Invoke-WebRequest $uri -Method $requestMethod   
-        "Returned status code for"+ $jobName + " is" + $response.StatusCode.tostring()
+        $interval      = $interval= [int] ($job.split(",")[4]).replace("Every","").replace("Minutes","").Trim()
+        if(((New-TimeSpan -Start $originalUTCTime -End $currentUTCtime).Minutes / $interval ) -is [int])
+        {
+            $response= Invoke-WebRequest $uri -Method $requestMethod   
+            "Returned status code for"+ $jobName + " is" + $response.StatusCode.tostring()
+        }       
     }  
 }
-
-# Write an information log with the current time.
